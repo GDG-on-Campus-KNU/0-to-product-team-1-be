6bd36a528df4a623f420bdf2bb75886a91c6a31f
Grow22 <uuu1856@naver.com>
2026-06-06 20:12:06 +0900

    feat: 일일 드릴 기록용 DailyDrillRecord DTO 생성

---
+++ be/src/main/java/com/gdg/backend/dto/response/DailyDrillRecord.java
@@ -0,0 +1,14 @@
+package com.gdg.backend.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+
+@Getter
+@Builder
+public class DailyDrillRecord {
+    private LocalDate date;
+    private String drillCategory;
+    private Boolean drillCompleted;
+}
